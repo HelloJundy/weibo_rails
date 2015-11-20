3bbd579121ecfeb71ae03993b54b5ff06d110bde
hellojundy <hellojundy@163.com>
2015-11-20 09:54:55 +0000

    add valid

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,47 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup            #setup方法會在測試方法运行前执行
+    @user = User.new(name:"hello", email: "hello@123.com");
+  end
+  
+  test "should be valid" do
+    assert @user.valid? 
+  end
+  
+  test "name should be present" do
+    @user.name = " "
+    assert_not @user.valid?     #因为在models中做了验证，如果名字为空的话  user.valid?  返回值为 false
+  end
+  
+  test "email should be present" do
+    @user.email=" "
+    assert_not @user.valid?
+  end
+  
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+  
+  test "email should not be too long" do
+    @user.email = "a" * 256
+    assert_not @user.valid?
+  end
+  
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+     @user.email = valid_address
+     assert @user.valid? , "#{valid_address.inspect} should be valid"
+    end
+  end
+  
+  test "email validation should reject invalid addresses" do
+    valid_addresses = %w[user@example,com user_at_foo.org user.name@example.foo@bar_baz.com foo@bar+baz.com]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert_not @user.valid?, "#{valid_address.inspect} should be invalid"
+    end
+  end
 end
