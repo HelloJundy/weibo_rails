e956a45ae3534e392aa0121f65fd8f0f42550650
hellojundy <hellojundy@163.com>
2015-11-24 02:40:26 +0000

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,12 +4,30 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-  get signup_path
-
-  assert_no_difference 'User.count' do
-    post users_path, user:{ name:"",
-                            email: "user@invalid",
-                            password:              "foo",
-                            password_confirmation: "bar"}
+ 
+  test "invalid signup imformation" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user:{ name:"",
+                              email: "user@invalid",
+                              password:              "foo",
+                              password_confirmation: "bar"}
+    end
+    assert_template "users/new"
+  end
+  test "valid signup imformation" do
+    get signup_path
+    name = "User"
+    email = "abc@abc.com"
+    password = "123456"
+    password_confirmation = "123456"
+    assert_difference 'User.count',1 do 
+      post_via_redirect users_path, user:{ name: name,
+                             email: email,
+                             password: password,
+                             password_confirmation: password_confirmation
+      }
+    end
+    assert_template "users/show"
   end
 end
