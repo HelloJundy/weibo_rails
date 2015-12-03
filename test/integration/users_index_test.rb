ff36449e0eb8a96ec68940341af76e420a90d99b
hellojundy <hellojundy@163.com>
2015-12-03 21:25:36 +0800

    finish delete user test

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -6,25 +6,25 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   # end
   def setup
     @admin = users(:admin)
-    @non_admin
+    @non_admin = users(:archer)
   end
   
   test "index as admin including pagination and delete links" do
-    log_in_as @admin
+    log_in_as(@admin)
     get users_path
     assert_template 'users/index'
-    assert_select 'div.pagination'
-    User.paginate(page: 1).each do |user|
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == admin
+      unless user == @admin
         assert_select 'a[href=?]', user_path(user), text: 'delete',
-                                                    method: delete
+                                                    method: :delete
       end
     end
   end
   
   test "index as non-admin" do
-    log_in_as @non_admin
+    log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
   end
