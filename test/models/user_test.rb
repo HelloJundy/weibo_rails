4753eb4760ec866507a8ef31a2ab208b2ebab64d
hellojundy <hellojundy@163.com>
2015-12-17 13:26:36 +0000

    #7 display microposts  * 渲染微博  * 实例微博  * 资料页面的微博测试

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -71,9 +71,9 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.authenticated?(:remember, '')
   end
   
-  test "associated micropost should be destroy" do
+  test "associated microposts should be destroy" do
     @user.save
-    @user.micropost.create!(content:"hello")
+    @user.microposts.create!(content:"hello")
     assert_difference 'Micropost.count', -1 do
       @user.destroy
     end
